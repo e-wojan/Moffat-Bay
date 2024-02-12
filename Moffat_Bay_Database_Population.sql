f9028b27e2065d441e40327f6e8fbf411cf607e4
e-wojan <122759502+e-wojan@users.noreply.github.com>
2024-02-12 16:05:31 -0600

    Updated .sql with additional room types.

--- Moffat_Bay_Database_Population.sql
+++ Moffat_Bay_Database_Population.sql
@@ -17,14 +17,26 @@ VALUES (0001, 'Allison', 'Rico', '281-228-5548', 'a.rico@gmail.com',  '$2y$10$5G
 	   (0003, 'Henry', 'Smith', '155-456-4562', 'H.Smith1@gmail.com', '$2y$10$Akp9UzM6hnXwIOmN8taJluysvECPUkpXT3zy.tF8IfiPoVPvpwoi.', '2417 Elm Wood Rd.', 'Apt 202', 'El Paso', 'Texas', '85843', 'USA');
        
 INSERT INTO Rooms (Room_ID, Room_Type, Availability, Rate)
-VALUES	(101, 'Double', FALSE, '120.00'),
-		(102, 'Queen', FALSE, '130.00'),
-        (103, 'King', FALSE, '140.00'),
-		(104, 'Queen', TRUE, '130.00'),
-        (105, 'Double', TRUE, '120.00'),
-		(106, 'King', TRUE, '140.00'),
-        (107, 'Double', TRUE, '120.00'),
-		(108, 'Queen', TRUE, '130.00');
+VALUES	(101, 'Double Full', FALSE, '120.00'),
+		(102, 'Queen', FALSE, '140.00'),
+        (103, 'Double Quen', FALSE, '130.00'),
+		(104, 'King', TRUE, '150.00'),
+        (105, 'Double Full', TRUE, '120.00'),
+		(106, 'Queen', TRUE, '140.00'),
+		(107, 'Double Queen', TRUE, '130.00'),
+        (108, 'King', TRUE, '150.00'),
+        (109, 'Double Full', TRUE, '120.00'),
+		(110, 'Quen', FALSE, '140.00'),
+		(111, 'Double Queen', TRUE, '130.00'),
+        (112, 'King', TRUE, '150.00'),
+        (113, 'Double Full', TRUE, '120.00'),
+        (114, 'Queen', TRUE, '140.00'),
+		(115, 'Double Queen', TRUE, '130.00'),
+        (116, 'King', TRUE, '150.00'),
+		(117, 'Double Full', TRUE, '120.00'),
+        (118, 'Queen', TRUE, '140.00'),
+		(119, 'Double Queen', TRUE, '130.00'),
+        (120, 'King', TRUE, '150.00');
 
 INSERT INTO Reservations (Confirmation_Number, Total_Guests, Date_of_Check_in, Date_of_Check_out, Reservation_Status, Special_Requests, Room_ID, User_ID)
 VALUES (12351, 4, '2024-02-21', '2024-02-24', 'Confirmed', '', 101, 0001),
