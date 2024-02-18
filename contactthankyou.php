405714f98d33b2d0d7d8bf601b2cbfa331d8fc6c
racwhi <128566218+racwhi@users.noreply.github.com>
2024-02-17 23:47:34 -0600

    Update contactthankyou.php
    updated contact us thank you page

--- contactthankyou.php
+++ contactthankyou.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>CSD460 Moffat Moffat Bay Lodge Contact Us Page</title>
 </head>
 <script>
 
 </script>
 <body>
-    
+    <?php
+  session_start();
+  include "register.php";
+  ?>
+
     <nav>
       <!--logo-->
       <!--top navigation bar-->
@@ -20,89 +24,78 @@
         <a href="./contact.php" class="active">Contact Us</a>
         <a href="./attractions.php">Attractions</a>
         <a href="./reservations.php">Reservations</a>
-       
+        <?php
+            if (isset($_SESSION['Email'])) {
+
+                ?>
+                
+                <a href="">My Reservations</a>
+                
+                
+                <?php
+            } else {
+                ?>
+                <a href="./registration_page.php">Registration</a>
+                <?php
+            }
+            ?>
+            <?php
+            if (isset($_SESSION['Email'])) {
+
+                ?>
+                <div id="logout"><a href="Logout.php" id="logoutlink">Logout</a></div>
+                <?php
+            } else {
+                ?>
+                <div class="navitemlogin"><a href="Login_Page.php">Login</a></div>
+                <?php
+            }
+            ?>
+
+
+            <!-- <div class="navitemlogin"><a href="./login.html" class="active">Login</a></div> -->
+            <div class="username">
+                <?php
+                if (isset($_SESSION['Email'])) {
+
+                    ?>
+                    <div class="" role="">Hello,
+                        <?php echo $_SESSION['Email'] . "!"; ?>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+
+            <div class="username">
+            <?php
+            if (isset($_SESSION['Email'])) {
+
+            ?>
+            <div class="" role="">Hello, 
+                <?php echo $_SESSION['Email']."!"; ?>
+            </div>
+            <?php
+            }
+            ?>
+            </div>
       </div>
     </nav>
 
     <div class="bannerimg">
       <p>Moffat Bay Lodge</p>
     </div>
-  
-   
-      
+     
     
+     
     <div class="container"> <!--container-->
     <div class="bodyinformation">
    <h2 id="registrationheader">Thank you from Moffat Bay Lodge</h2>
-  <?php 
-$errors = '';
-$contactemail = 'racwhite2@my365.bellevue.edu';//<-----Put Your email address here.
-
-$fullName = isset($_POST['fullName']) ? $_POST['fullName'] : '';
-$emailUser = isset($_POST['emailUser']) ? $_POST['emailUser'] : '';
-$messageUser = isset($_POST['messageUser']) ? $_POST['messageUser'] : '';
-
-if (isset($_POST['send'])){
-
-if(empty($_POST['fullName'])  || 
-   empty($_POST['emailUser']) || 
-   empty($_POST['messageUser']))
-{
-    $errors .= "\n All fields required, please try again";
-}
-
-$fullname = $_POST['fullName']; 
-$emailUser = $_POST['emailUser']; 
-$messageUser = $_POST['messageUser']; 
-
-if (!filter_var($emailUser, FILTER_VALIDATE_EMAIL))
-
-{
-    $errors .= "\n Invalid email address, please try again";
-}
-
-if(empty($errors))
-{
-	$to = $contactemail; 
-	$email_subject = "Contact form submission: $fullName";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $fullName \n Email: $emailUser \n Message \n $messageUser"; 
-	
-	$headers = "From: $contactemail\n"; 
-	$headers .= "Reply-To: $emailUser";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
-
-}
-
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact Thank You</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-
-
-
-
-
-    
-
-
-<div class="form-container">
   <p style="text-align:center;">Thank you for submitting the contact form, we will contact you soon!
 </p>
 
 </div> 
+</div>
 
 <div class="footer">
     <p>This website was created as a class assignment
